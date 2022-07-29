626f583e7c260c73fcb7e39400269e4a352f8189
eunkeeee <eunkeeeee@gmail.com>
2022-07-29 13:21:03 +0900

    6.1 useEffect => 한번만 실행하고 싶은 코드

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => console.log("i run only once(CALL THE API)");
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1 className={styles.title}>{counter}</h1>
