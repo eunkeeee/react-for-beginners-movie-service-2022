19632846a4299c093ee9f9ff316d2615cb1f6caa
eunkeeee <eunkeeeee@gmail.com>
2022-07-29 13:34:55 +0900

    6.2 Deps => 특정 State가 바뀔 때만 render 되는 함수들을 지정해보자

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
-  const iRunOnlyOnce = () => console.log("i run only once(CALL THE API)");
-  useEffect(iRunOnlyOnce, []);
+  useEffect(() => console.log("CALL THE API"), []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >= 2) {
+      console.log("SEARCH FOR ", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      ></input>
       <h1 className={styles.title}>{counter}</h1>
       <Button onClick={onClick} text="Click me" />
     </div>
