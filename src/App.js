84facbc6bfe73c4c0128f3cc5110504089662101
eunkeeee <eunkeeeee@gmail.com>
2022-07-29 14:50:46 +0900

    6.2 useEffect 예시 dependency까지 추가

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-  useEffect(() => console.log("CALL THE API"), []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >= 2) {
-      console.log("SEARCH FOR ", keyword);
-    }
-  }, [keyword]);
+  useEffect(() => console.log("I run only once."), []);
+  useEffect(() => console.log("I run when 'keyword' changes"), [keyword]);
+  useEffect(() => console.log("I run when 'counter' changes"), [counter]);
+  useEffect(
+    () => console.log("I run when keyword & counter changes"),
+    [keyword, counter]
+  );
 
   return (
     <div>
