b2e3c06395a2cdda9b4e98340334303b90f81f56
eunkeeee <eunkeeeee@gmail.com>
2022-08-01 14:09:16 +0900

    6.4 Cleanup Function

--- src/App.js
+++ src/App.js
@@ -2,29 +2,20 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => console.log("I run only once."), []);
-  useEffect(() => console.log("I run when 'keyword' changes"), [keyword]);
-  useEffect(() => console.log("I run when 'counter' changes"), [counter]);
-  useEffect(
-    () => console.log("I run when keyword & counter changes"),
-    [keyword, counter]
-  );
+function Hello() {
+  useEffect(() => {
+    console.log("created :)");
+    return () => console.log("destroyed :(");
+  }, []);
+}
 
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1 className={styles.title}>{counter}</h1>
-      <Button onClick={onClick} text="Click me" />
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
