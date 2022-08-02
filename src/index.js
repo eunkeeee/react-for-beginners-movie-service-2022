5756a7eddc93cdcb0dcf3d3c6c6e5a6544d17529
eunkeeee <eunkeeeee@gmail.com>
2022-08-02 12:51:55 +0900

    7.3 Movie List 데려오기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App3.js";
+import App from "./App4.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
